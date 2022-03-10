80d3d24aaeb754a02aeb7a832b71a93380c10958
Afford7234 <lalka.ta71@gmail.com>
2022-03-10 16:52:31 +0300

    comment

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleApp3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Student library = new Student();
+
+            library.name = "Полоцкая городская";
+
+            library.kurs = 3;
+
+            library.pol = 1;
+
+
+            Console.WriteLine($"{library.name} {library.kurs} {library.pol}");
+
+            Student lib1 = new Student("Полоцкая центральная", 2, 0);
+
+            Console.WriteLine($"{library.name} {library.kurs} {library.pol}");
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
---
+++ ConsoleApp3/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Student
+    {
+        public string name { get; set; }
+        public int kurs { get; set; }
+        public int pol { get; set; }
+        public Student() { }
+        public Student(string name, int kurs, int pol)
+
+        {
+
+            name = this.name;
+
+            kurs = this.kurs;
+
+            pol = this.pol;
+
+        }
+
+    }
+}
